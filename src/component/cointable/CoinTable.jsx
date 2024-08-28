97b84a43abd31351129171c6d53f1d5b48ba3398
Shrey <guptashrey163@gmail.com>
2024-08-28 15:08:52 +0530

    react query setup for seemless api calls

--- src/component/cointable/CoinTable.jsx
+++ src/component/cointable/CoinTable.jsx
@@ -1,19 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getCoinData } from "../../services/FetchCoinData";
+import { useQuery } from "react-query";
 
 function CoinTable(){
 
+    const [page,setPage]=useState(1);
+
+const {data,isLoading,isError,error}=useQuery(['coin',page],()=>getCoinData(page,'usd'),{
+    retry:2,
+    retryDelay:1000,
+    cacheTime:1000*60*5,
+})
 useEffect(()=>{
-console.log("coin table mounted");
-getCoinData(1,'usd');
-},[])
+    console.log(data);
+},[data])
+
+if(isLoading){
+   return <div>loading...</div>;
+
+}
+
+if(isError){
+   return  <div>Error :{error.message}</div>;
+}
+
 
     return (
-        <>
-        CoinTable
+        
+        <> coin table <button onClick={()=>setPage(page+1)}>click {page} </button> </>
 
         
-        </>
+       
     )
 }
 
